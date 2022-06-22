0c1a23e54de345da868b745f8dc4a0de64017718
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-06-22 11:06:43 +0530

    Add files via upload

---
+++ strings_method.py
@@ -0,0 +1,118 @@
+# a='hello'
+# print(a.capitalize()) # Hello
+
+# b='Hello'
+# print(b.casefold()) # hello
+
+# c='Niharika'
+# print(c.center(10)) #  Niharika
+
+# d='i love dog,dog is my favorite pet'
+# print(d.count('dog')) # 2
+
+# e= 'hi,good morning.'
+# print(e.endswith('.')) # true
+
+# f='n\ti\th\ta\tr\ti\tk\ta'
+# print(f.expandtabs(0)) # niharika
+
+# g='happy birthday'
+# print(g.find('b')) # 6
+
+# h= 'Hello, good morning'
+# print(h.index('g')) # 7
+
+# i='system16'
+# print(i.isalnum()) # true,if all the characters in string are alphanumeric otherwise false
+# j='system 16'
+# print(j.isalnum()) # flase
+
+# k='niharika'
+# # print(k.isalpha()) # true
+# l='niharika10'
+# print(l.isalpha()) # false
+
+# m='12345'
+# print(m.isdecimal()) # true
+# n='g12345'
+# print(n.isdecimal()) # false
+
+# o='5678'
+# print(o.isdigit()) # true,if all the characters are digits otherwise false
+
+# p='class'
+# print(p.isidentifier()) # true
+# q='2class'
+# print(q.isidentifier()) # false
+
+# r='hello'
+# print(r.islower()) # true
+
+# s='HELLO'
+# print(s.isupper()) # True
+
+# t='0123'
+# print(t.isnumeric()) # true
+
+# u=' '
+# print(u.isspace()) # true
+# v='-'
+# print(v.isspace()) # false
+
+
+# w='Hello, World'
+# print(w.istitle()) # true
+# x='HELLO, WORLD'
+# print(x.istitle()) # false
+
+
+
+
+# a='hyderabad'
+# print(a.ljust(20,'0')) # hyderabad00000000000
+
+# b='Hello, World'
+# print(b.lower()) # hello, world
+
+# c='......,,,,,Niharika....'
+# print(c.lstrip('.,')) # Niharika....
+
+# d='can'
+# mytable=d.maketrans('c', 'v')
+# print(d.translate(mytable)) # van
+
+# e='i ate mango in the morning'
+# print(e.partition('mango')) # 'i ate','mango', 'in the morning'
+
+# f='i like rose'
+# print(f.replace('rose', 'lilly')) # i like lilly
+
+# g='Hello, World'
+# print(g.rfind('o')) # 8
+
+# h='Hyderabad'
+# print(h.rjust(20,'0')) # 00000000000Hyderabad
+
+# i='hello,world'
+# print(i.upper()) #HELLO,WORLD
+
+# k='..Niharika..kkll'
+# print(k.rstrip('.kl')) #..Niharika
+
+# l='Hello,world'
+# print(l.rindex('w')) # 6 
+
+# m='hello,Good morning'
+# print(m.title()) #Hello,Good Morning
+
+# n='my name is niharika'
+# print(n.split()) # 'my' 'name' 'is' 'niharika'
+
+# o='Hello, Good morning'
+# print(o.swapcase()) # hELLO, gOOD MORNING
+
+# p='my favourite colour is black'
+# print(p.startswith('my')) # true
+
+# q='...ffffhhhhdddsystem...'
+# print(q.strip('.fhd')) #system
